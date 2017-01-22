2da4bd36e895848a1227ec7b4af6995a10c3dffa
Stefan Wollny <sw@secomba.com>
2017-01-22 08:31:13 +0100

    perl language defaults

--- exports.sh
+++ exports.sh
@@ -5,3 +5,7 @@ export PATH=/bin:/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$PATH
 export PATH=~/.emsdk:$PATH
 export ANDROID_HOME=/Users/sw/Library/Android/sdk
 export HISTCONTROL=ignoreboth:erasedups
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
